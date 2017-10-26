812b9d2919fd1f3c7e747ddf493dbf4c51967426
zkself <zkself@126.com>
2017-10-26 21:35:06 +0800

    update handle json file

---
+++ source code/handleJson.py
@@ -0,0 +1,64 @@
+#-*- coding:utf-8 -*-
+import json
+import os
+import sys
+reload(sys)
+sys.setdefaultencoding("utf-8")
+
+
+def readFile():
+    cpath = os.getcwd()  # 获取当前工作目录
+    fpath = cpath + '/cache' + '/subscribe.json'
+    if os.path.exists(fpath):  # 判断文件是否存在
+        f = open(fpath)
+        content = f.read()
+        f.close()
+        return content
+    else:
+        f = open(fpath, 'w')  # 不存在就新建一个
+        f.close()
+        return 0
+
+
+def writeFile(content):
+    cpath = os.getcwd()
+    fpath = cpath + '/cache' + '/subscribe.json'
+    f = open(fpath, 'w')
+    f.write(content)
+    f.close()
+
+
+def updateSubscribeList(name, address, lastEpisode, updatetime):
+    content = readFile()
+    if content:
+        subscribe_list = json.loads(content)
+    else:
+        subscribe_list = {
+            'list': [],
+            'total': 0
+        }
+    new_subscribe = {'name': name, 'address': address,
+                     'lastEpisode': lastEpisode, 'updatetime': updatetime}
+    subscribe_list['list'].append(new_subscribe)
+    subscribe_list['total'] += 1
+    writeFile(json.dumps(subscribe_list))
+
+
+def deleateSubscribeList(name):
+    content = readFile()
+    if content:
+        subscribe_list = json.loads(content)
+    list = subscribe_list['list']
+    index = 0
+    for i in range(len(list)):
+        if name in list[i]:
+            index = i
+    del list[index]
+    subscribe_list['list'] = list
+    subscribe_list['total'] -= 1
+    writeFile(json.dumps(subscribe_list))
+
+updateSubscribeList('abc','www.baidu.com',5,'Monday')
+
+
+
